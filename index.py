dd3136cf338707be429ed2655afc4f58a96cbcf2
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-24 12:07:01 -0500

    Add summoner-v4 endpoint

--- index.py
+++ index.py
@@ -13,21 +13,28 @@ with open(dir + '/config.yml') as c:
     config = yaml.load(c, Loader=yaml.FullLoader)
     OPENWEATHERMAP_API_KEY = config['OPENWEATHERMAP']['API_KEY']
     GC_GSTRATE_API_KEY = config['GC']['GSTRATE']['API_KEY']
+    RIOT_API_KEY = config['RIOT']['API_KEY']
 
 
 @app.route('/')
 def index():
-    return '<h1>ORIENT API</h1>'
+    return '<h1 style="text-align: center">ORIENT API</h1>'
+
+
+@app.route('/gc/gstrate', methods=['GET'])
+# https://cra-arc.api.canada.ca/en/detail?api=GSTRate
+def gc_gstrate():
+    return json.loads(requests.get(f'https://gstrate-cra-arc.api.canada.ca/ebci/ghnf/api/ext/v1/rates', headers={'user-key': GC_GSTRATE_API_KEY}).content)
 
 
 @app.route('/openweathermap/weather', methods=['GET'])
 # https://openweathermap.org/current
 def openweathermap_weather():
     if request.args.get('q'):
-        return json.loads(requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={request.args.get('q')}&appid={OPENWEATHERMAP_API_KEY}").content)
+        return json.loads(requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={request.args.get("q")}&appid={OPENWEATHERMAP_API_KEY}').content)
 
 
-@app.route("/gc/gstrate", methods=['GET'])
-# https://cra-arc.api.canada.ca/en/detail?api=GSTRate
-def gc_gstrate():
-    return json.loads(requests.get(f"https://gstrate-cra-arc.api.canada.ca/ebci/ghnf/api/ext/v1/rates", headers={"user-key": GC_GSTRATE_API_KEY}).content)
+# https://developer.riotgames.com/apis
+@app.route('/lol/summoner/v4/summoners/<string:region>/by-name/<string:summonerName>', methods=['GET'])
+def riot_get_summoner_by_summoner_name(region, summonerName):
+    return json.loads(requests.get(f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}', headers={'X-Riot-Token': RIOT_API_KEY}).content)
