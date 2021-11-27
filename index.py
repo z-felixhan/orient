1ac9c310f324739c97313578a766e5614664ae0b
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-27 02:57:22 -0500

    Add NOC

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS
+import data
 import os
 import requests
 import yaml
@@ -11,15 +12,20 @@ CORS(app)
 # Load config
 with open(dir + '/config.yml') as c:
     config = yaml.load(c, Loader=yaml.FullLoader)
-    OPENWEATHERMAP_API_KEY = config['OPENWEATHERMAP']['API_KEY']
     GC_GSTRATE_API_KEY = config['GC']['GSTRATE']['API_KEY']
+    OPENWEATHERMAP_API_KEY = config['OPENWEATHERMAP']['API_KEY']
     RIOT_API_KEY = config['RIOT']['API_KEY']
+
+# Load data
+NOC = data.NOC
 
 
 @app.route('/')
 def index():
     return '<h1 style="text-align: center">ORIENT API</h1>'
 
+
+### WRAPPERS ###
 
 @app.route('/gc/gstrate', methods=['GET'])
 # https://cra-arc.api.canada.ca/en/detail?api=GSTRate
@@ -35,6 +41,13 @@ def openweathermap_weather():
 
 
 # https://developer.riotgames.com/apis
-@app.route('/lol/summoner/v4/summoners/<string:region>/by-name/<string:summonerName>', methods=['GET'])
-def riot_get_summoner_by_summoner_name(region, summonerName):
+@app.route('/riotgames/lol/summoner/v4/summoners/<string:region>/by-name/<string:summonerName>', methods=['GET'])
+def riotgames_lol_get_summoner_by_summoner_name(region, summonerName):
     return json.loads(requests.get(f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}', headers={'X-Riot-Token': RIOT_API_KEY}).content)
+
+
+### CUSTOM ###
+
+@app.route('/noc/v1/2016')
+def gc_noc_2016():
+    return jsonify(NOC)
